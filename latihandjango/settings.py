faf0b0771a55881bf09aebdee9290ffda9990505
akbarebw <39755833+akbarebw@users.noreply.github.com>
2022-12-21 15:20:28 +0800

    Update settings.py

--- latihandjango/settings.py
+++ latihandjango/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'base.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
